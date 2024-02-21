f10e01706571c285582cc8a43623a0385056fe50
Maximilian Braun <maxbraunbm@gmail.com>
2024-02-21 16:43:42 +0100

    not styled yet

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :category, :address, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+puts "Creating restaurants..."
+dal_cavaliere = {name: "Dal Cavaliere", address: "Weißenburger Straße 1", phone_number: "+49 1234 5678", category: "italian"}
+cafe_miu = {name: "Café Miu", address: "Sedanstraße 39, 81667 München", phone_number: "+49 1234 9978", category: "chinese"}
+giesinger_stehausschank = {name: "Giesinger Bräu - Stehausschank", address: "Prälat-Zistl-Straße 4, 80331 München", phone_number: "+49 1111 3333", category: "japanese"}
+plaenty_sandwiches = {name: "plänty SANDWICHES", address: "Altheimer Eck 9, 80331 München", phone_number: "+49 999 1111", category: "japanese"}
+tahdig = {name: "Tahdig", address: "Thierschstraße 35, 80538 München", phone_number: "+49 222222", category: "japanese"}
+
+[dal_cavaliere, cafe_miu, giesinger_stehausschank, plaenty_sandwiches, tahdig].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  nice = { rating: 4, content: "War geil", restaurant: restaurant}
+  shit = { rating: 2, content: "Geht besser", restaurant: restaurant}
+  puts "Created #{restaurant.name}"
+  [nice, shit].each do |rev|
+    review = Review.create!(rev)
+    puts "#{restaurant.name}: #{review.content}"
+  end
+  end
